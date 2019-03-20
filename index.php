8a84653a77d11b4fee42aac44a8407d42ae1dd1b
VBARobotics <45722764+VBARobotics@users.noreply.github.com>
2019-03-20 18:31:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,17 @@ if (isset($_POST['LightOFF']))
 exec("sudo python /var/www/LED/led_off.py");
 }
 ?>
+ <P>
+  
  <center>
+  <P>
+   Power On LED Strip</P
 <form method="post">
 <button class="btn" name="LightON">Light ON</button> <P>
  <button class="btn" name="LightOFF">Light OFF</button><br><br></center>
+   <P>
+    <P>
+     <button class="btn" name="ShoulderLeft">Shoulder Left</button>
     <br><br><br><br>
 </form>
 </html>
