1c861f0addbbe7041d19c57c3d095eb17c5d50c0
VBARobotics <45722764+VBARobotics@users.noreply.github.com>
2019-03-22 12:15:54 -0500

    Update index.php

--- index.php
+++ index.php
@@ -9,18 +9,22 @@ if (isset($_POST['LightOFF']))
 {
 exec("sudo python /var/www/LED/led_off.py");
 }
+ if (isset($_POST['RobotDemo']))
+{
+exec("sudo python /var/www/arm/arm-motions.py motions.txt");
+}
 ?>
  <P>
-  
+    <P>
+    <P>
+     <button class="btn" name="RobotDemo">Robot Arm Demo</button>
  <center>
   <P>
    Power On LED Strip</P>
 <form method="post">
 <button class="btn" name="LightON">Light ON</button> <P>
  <button class="btn" name="LightOFF">Light OFF</button><br><br></center>
-   <P>
-    <P>
-     <button class="btn" name="ShoulderLeft">Shoulder Left</button>
+ 
     <br><br><br><br>
 </form>
 </html>
