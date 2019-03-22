68daa9367edcd42bd62469ecb27aad7132040b86
VBARobotics <45722764+VBARobotics@users.noreply.github.com>
2019-03-22 12:20:33 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,15 +15,17 @@ exec("sudo python3 /var/www/arm/arm-motions.py /var/www/arm/motions.txt");
 }
 ?>
  <P>
-    <P>
-    <P>
-     <button class="btn" name="RobotDemo">Robot Arm Demo</button>
+    
  <center>
   <P>
    Power On LED Strip</P>
 <form method="post">
 <button class="btn" name="LightON">Light ON</button> <P>
- <button class="btn" name="LightOFF">Light OFF</button><br><br></center>
+ <button class="btn" name="LightOFF">Light OFF</button><br><br>
+ <P>
+    <P>
+     <button class="btn" name="RobotDemo">Robot Arm Demo</button>
+ </center>
  
     <br><br><br><br>
 </form>
